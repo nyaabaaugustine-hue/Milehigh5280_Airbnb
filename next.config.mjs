995eb3b0fbd26c992b7645a0e060f0980be77abe
JobBox Developer <user@example.com>
2026-04-07 21:20:46 +0000

    fixednow

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,15 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
